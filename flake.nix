bfbd1c4fe89204e4036a0338910f79cb1bf58392
Pepijn Overbeeke <pepijn.overbeeke@gmail.com>
2022-03-26 19:22:21 +0100

    Add build to the flake

--- flake.nix
+++ flake.nix
@@ -6,21 +6,37 @@
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem
       (system:
-        let pkgs = nixpkgs.legacyPackages.${system}; in
-        {
-          devShell = pkgs.mkShell
-            {
-              nativeBuildInputs = with pkgs; [
-                xorg.libX11
-                xorg.libX11.dev
-                xorg.libX11.out
-                xorg.xorgproto
-                xorg.libXinerama
-                xorg.libXinerama.dev
-                xorg.libXft
-                xorg.libXft.dev
-              ];
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          buildInputs = with pkgs; [
+            xorg.libX11
+            xorg.libX11.dev
+            xorg.libX11.out
+            xorg.xorgproto
+            xorg.libXinerama
+            xorg.libXinerama.dev
+            xorg.libXft
+            xorg.libXft.dev
+          ];
+        in
+        rec {
+          packages = {
+            dmenu = pkgs.stdenv.mkDerivation {
+              inherit buildInputs;
+              name = "dmenu";
+              src = self;
+              installPhase = ''
+                mkdir -p $out/bin
+                cp dmenu $out/bin
+                cp dmenu_path $out/bin
+                cp dmenu_run $out/bin
+              '';
             };
+          };
+          defaultPackage = packages.dmenu;
+          devShell = pkgs.mkShell {
+            nativeBuildInputs = buildInputs;
+          };
         }
       );
 }
